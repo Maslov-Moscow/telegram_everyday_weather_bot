fe51d48e59bc4306afce26ed4926424c0c10f921
Maslov-Moscow <trubraver@gmail.com>
2020-05-27 21:32:40 +0300

    'test3'

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import requests
 import telegram
 import os
 
-print(telegram.__version__)
 
-# TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
-# CHAT_ID = os.environ.get('CHAT_ID')
-#
-#
-# def send():
-#     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-#     return bot.send_message(chat_id=CHAT_ID, text='Я живой!!')
-#
-#
-# if __name__ == '__main__':
-#     send()
+
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+CHAT_ID = os.environ.get('CHAT_ID')
+
+
+def send():
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    return bot.send_message(chat_id=CHAT_ID, text='Я живой!!')
+
+
+if __name__ == '__main__':
+    send()
